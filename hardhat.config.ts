31cb0c8e6a142d445a25bd63cd822d5dc2fa94d5
“Kyle <KyleMcodes@gmail.com>
2025-12-22 14:48:59 -0800

    changed version in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -97,7 +97,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.19",
+    version: "^0.8.18",
     settings: {
       metadata: {
         // Not including the metadata hash
